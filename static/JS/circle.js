3140bc96494a010e9110e0a230007ba16031717a
jwo29 <55731054+jwo29@users.noreply.github.com>
2020-12-11 18:55:07 +0900

    Update circle.js

--- static/JS/circle.js
+++ static/JS/circle.js
@@ -5,6 +5,7 @@ if(!canvas){
 }
 
 var context = canvas.getContext("2d");
+
 var raf = null; // 애니메이션 컨트롤
 var PI = Math.PI * 2; // 원주율
 
@@ -20,86 +21,76 @@ class Circle extends Vector {
     radius = 0;
     type = "circle";
 
-    colorCode = "#ffffff";
-
     // 생성자
-    constructor (x, y, radius, dx, dy, r, g, b) {
-        super(); // 상속
+    constructor (x, y, radius, dx, dy, r, g, b, info) {
+        super(); // Vector 상속
         this.x = x;
         this.y = y;
         this.radius = radius;
+
+        // (dx == 0 OR dy == 0)인 경우가 없도록 함
         this.dx = dx || 1;
         this.dy = dy || -1;
+        
         this.red = r;
         this.green = g;
         this.blue = b;
+        
+        this.info = info; // 'H:M count', count: 해당 시간의 인체 감지 횟수 
     }   
 
     draw() {
+        /* 원 그리기 */
+
         context.beginPath();
         context.arc(this.x, this.y, this.radius, 0, PI);
         context.fillStyle = 
-            `rgba(${this.red}, ${this.green}, ${this.blue}, 0.35)`;
-        // context.stroke();
+            `rgba(${this.red}, ${this.green}, ${this.blue}, 0.35)`; // 투명도: 0.35
         context.fill();
     }
     
     acc() {
-        const posX = this.x + this.dx;
-        const posY = this.y + this.dy;
+        /* 원 움직이기 */
+
+        const posX = this.x + this.dx; // 현재의 x좌표에 dx만큼 더함
+        const posY = this.y + this.dy; // 현재의 y좌표에 dy만큼 더함
     
-        if (posX > canvas.width - this.radius || posX < this.radius) {
+        // 원의 경계가 프레임(canvas)을 벗어나느 값이면 이동 방향 바꿈
+        if (posX > canvas.width - this.radius || posX < this.radius)
           this.dx = -this.dx;
-        }
     
-        if (posY > canvas.height - this.radius || posY < this.radius) {
+        if (posY > canvas.height - this.radius || posY < this.radius)
           this.dy = -this.dy;
-        }
 
-        this.x += this.dx;
-        this.y += this.dy;
+        this.x += this.dx; // x좌표 위치 갱신
+        this.y += this.dy; // y좌표 위치 갱신
     
-        this.draw();
+        this.draw(); // 변경된 내용으로 원 다시 그리기
     }
 
-    to(x, y) {
-        this.x = x;
-        this.y = y;
-    
-        this.draw();
-    }
+    text() {
+        /* 원의 중심에 텍스트, 그리기 */
 
-    text(comment) {
-        const text = comment || `${this.x}, ${this.y}`;
+        // 글자가 원의 중심에 위치하도록 함
+        context.textBaseline = "middle";
+        context.textAlign = "center";
+
         context.strokeStyle = "#242424";
-        context.font = "normal 12px Roboto";
+        context.font = "normal 20px Roboto";
         context.lineWidth = 1;
-        context.strokeText(text, this.x, this.y);
-    }
-
-    line(){
-        context.strokeStyle = "#242424";
-        context.lineWidth = 0.5;
-
-        context.beginPath();
-        context.moveTo(this.x, canvas.height);
-        context.lineTo(this.x, 0);
-        context.stroke();
-
-        context.beginPath();
-        context.moveTo(0, 0);
-        context.lineTo(0, this.y);
-        context.lineTo(canvas.width, this.y)
-        context.stroke();
+        
+        context.strokeText(this.info, this.x, this.y); // 원 위의 시간, 감지 횟수 정보 텍스트를 그림
     }
 }
 
 function play() {
+    /* 애니메이션 시작 */
     context.clearRect(0, 0, canvas.width, canvas.height);
     layer();
     raf = window.requestAnimationFrame(play);
 }
 
 function pause() {
+    /* 애니메이션 중지 */
     window.cancelAnimationFrame(raf);
 }
